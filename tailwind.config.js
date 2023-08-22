29115fb920de71c3e23fd7bfca296393e4a45665
emmabaka <bakaemma88@gmail.com>
2023-08-22 11:14:31 +0300

    init: start project

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      hero: "url('/public/hero.png')",
+    },
+    screens: {
+      mobile: "320px",
+      tablet: "768px",
+      desktop: "1280px",
+    },
+    colors: {
+      white: "#fff",
+      red: "#ff5757",
+      buttonHover: "rgba(255, 255, 255, 0.2)",
+      backgroundInput: "rgba(255, 255, 255, 0.05)",
+      backgroundDefault: "rgba(255, 255, 255, 0.10)",
+    },
+    fontFamily: {
+      inter: ["Inter", " sans-serif"],
+    },
+    fontSize: {
+      10: "10px",
+      12: "12px",
+      13: "13px",
+      14: "14px",
+      16: "16px",
+      18: "18px",
+      20: "20px",
+      22: "22px",
+      28: "28px",
+      30: "30px",
+      32: "32px",
+      33: "33px",
+      37: "37px",
+      40: "40px",
+      43: "43px",
+      67: "67px",
+      98: "98px",
+    },
+  },
+
+  plugins: [],
+};
