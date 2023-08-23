f75090a09a8f87b78e7d73e3c6d9934a64f29828
emmabaka <bakaemma88@gmail.com>
2023-08-23 18:27:47 +0300

    feat: add new styles for validation errors

--- src/components/ChooseUsForm/ChooseUsForm.jsx
+++ src/components/ChooseUsForm/ChooseUsForm.jsx
@@ -1,15 +1,16 @@
 "use client";
 import { useForm } from "react-hook-form";
 import InputMask from "react-input-mask";
+import error from "../../../public/x.svg";
 import s from "./ChooseUsForm.module.css";
-
+import Image from "next/image";
 
 const ChooseUsForm = () => {
   const {
     handleSubmit,
     register,
     formState: { errors },
-    formState,
+    reset,
   } = useForm({
     mode: "onBlur",
     defaultValues: {
@@ -23,9 +24,9 @@ const ChooseUsForm = () => {
   });
   const onSubmit = (values) => {
     console.log(values);
+    reset();
   };
-  console.log(errors);
-  console.log(formState);
+
   return (
     <div className="py-[56px] px-5">
       <p className="text-14 text-white font-extralight leading-5 max-w-[179px] ml-auto pb-[24px]">
@@ -35,12 +36,17 @@ const ChooseUsForm = () => {
       </p>
       <form className="bg-transparent" onSubmit={handleSubmit(onSubmit)}>
         <div className="relative flex flex-col gap-1 pb-4">
-          <label className={s.label} htmlFor="name">
+          <label
+            className={`${errors.name ? "text-red" : "text-white"} ${s.label}`}
+            htmlFor="name"
+          >
             Full name
           </label>
           <div className="relative">
             <input
-              className={`${s.input} px-2`}
+              className={`${errors.name ? "text-red" : "text-white"} ${
+                s.input
+              } px-2`}
               type="text"
               {...register("name", {
                 required: "Required",
@@ -53,16 +59,26 @@ const ChooseUsForm = () => {
               id="name"
               placeholder="John Smith"
             />
-            <p className={s.error}>{errors.name && errors.name.message}</p>
+            {errors.name && (
+              <p className={s.error}>
+                <Image src={error} alt="error" width={18} height={18} />
+                {errors.name.message}
+              </p>
+            )}
           </div>
         </div>
         <div className="relative flex flex-col gap-1 pb-4">
-          <label className={s.label} htmlFor="email">
+          <label
+            className={`${errors.email ? "text-red" : "text-white"} ${s.label}`}
+            htmlFor="email"
+          >
             E-mail
           </label>
           <div className="relative">
             <input
-              className={`${s.input} px-2`}
+              className={`${errors.name ? "text-red" : "text-white"} ${
+                s.input
+              } px-2`}
               type="email"
               {...register("email", {
                 required: "Required",
@@ -74,7 +90,12 @@ const ChooseUsForm = () => {
               id="email"
               placeholder="johnsmith@email.com"
             />
-            <p className={s.error}>{errors.email && errors.email.message}</p>
+            {errors.email && (
+              <p className={s.error}>
+                <Image src={error} alt="error" width={18} height={18} />
+                {errors.email.message}
+              </p>
+            )}
           </div>
         </div>
         <div className="flex flex-col gap-1 pb-4">
@@ -92,16 +113,21 @@ const ChooseUsForm = () => {
           />
         </div>
         <div className="flex flex-col gap-1 pb-4">
-          <label className={s.label} htmlFor="phone">
+          <label
+            className={`${errors.phone ? "text-red" : "text-white"} ${s.label}`}
+            htmlFor="phone"
+          >
             Phone
           </label>
           <div className="relative">
             <InputMask
-              className={`${s.input} pl-14 pr-2`}
+              className={`${errors.name ? "text-red" : "text-white"} ${
+                s.input
+              } pl-14 pr-2`}
               type="text"
               mask="(999) 99 99 999"
               {...register("phone", {
-                required: false,
+                required: "Required",
                 pattern: {
                   value: /^\(\d{3}\) \d{2} \d{2} \d{3}$/,
                   message: "Incorrect phone",
@@ -114,7 +140,12 @@ const ChooseUsForm = () => {
             <span className="absolute left-2 top-2/4 -translate-y-1/2 text-20 text-white font-extralight leading-6">
               + 38
             </span>
-            <p className={s.error}>{errors.phone && errors.phone.ref.value}</p>
+            {errors.phone && (
+              <p className={s.error}>
+                <Image src={error} alt="error" width={18} height={18} />
+                {errors.phone.message}
+              </p>
+            )}
           </div>
         </div>
         <div className="flex flex-col gap-1 pb-4">
@@ -132,9 +163,17 @@ const ChooseUsForm = () => {
           />
         </div>
         <div className="pb-4">
-          <input type="checkbox" name="confirm" id="confirm" />
+          <input
+            type="checkbox"
+            {...register("confirm", {
+              required: true,
+            })}
+            id="confirm"
+          />
           <label
-            className="text-12 text-white font-extralight leading-[22px]"
+            className={`${
+              errors.confirm ? "text-red" : "text-white"
+            } text-12 font-extralight leading-[22px]`}
             htmlFor="confirm"
           >
             I confirm my consent to the processing of personal data.
