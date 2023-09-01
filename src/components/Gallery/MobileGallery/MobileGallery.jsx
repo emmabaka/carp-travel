4abf660cc7f103015a5c229ae47b861ae8f90e34
Emma Baka <112560476+emmabaka@users.noreply.github.com>
2023-09-01 14:44:09 +0300

    feat: add mobile gallery slider

--- src/components/Gallery/MobileGallery/MobileGallery.jsx
+++ src/components/Gallery/MobileGallery/MobileGallery.jsx
@@ -1,47 +1,82 @@
 'use client';
 import Image from 'next/image';
+import useEmblaCarousel from 'embla-carousel-react';
 import forest from '../../../../public/gallery-forest.jpg';
 import lake from '../../../../public/gallery-lake.jpg';
 import mountain from '../../../../public/gallery-mountain.jpg';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import fog from '../../../../public/gallery-fog.jpg';
+import s from './MobileGallery.module.css';
 
 const MobileGallery = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    axis: 'y',
+    loop: true,
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    containScroll: 'keepSnaps',
+    watchSlides: false,
+    watchResize: false,
+    dragFree: true,
+  });
+
+  // const scrollPrev = useCallback(() => {
+  //   if (emblaApi) emblaApi.scrollPrev();
+  // }, [emblaApi]);
+
+  // const scrollNext = useCallback(() => {
+  //   if (emblaApi) emblaApi.scrollNext();
+  // }, [emblaApi]);
+
   return (
-    <ul className=' flex flex-col gap-6 items-center'>
-      <li>
-        <Image src={mountain} alt='Mountain' width={280} height={187} />
-      </li>
-      <li>
-        <Image src={lake} alt='Lake' width={280} height={187} />
-      </li>
-      <li>
-        <Image src={forest} alt='Forest' width={280} height={187} />
-      </li>
-    </ul>
+    <div className={s.embla}>
+      <div className={s.emblaViewport} ref={emblaRef}>
+        <div className={s.emblaContainer}>
+          <div className={s.emblaSlide}>
+            <Image
+              className=' m-auto'
+              src={lake}
+              alt='Lake'
+              width={280}
+              height={187}
+            />
+          </div>
+          <div className={s.emblaSlide}>
+            <Image
+              className=' m-auto'
+              src={forest}
+              alt='Forest'
+              width={280}
+              height={187}
+            />
+          </div>
+          <div className={s.emblaSlide}>
+            <Image
+              className=' m-auto'
+              src={fog}
+              alt='Forest'
+              width={280}
+              height={187}
+            />
+          </div>
+          <div className={s.emblaSlide}>
+            <Image
+              className=' m-auto'
+              src={mountain}
+              alt='Mountain'
+              width={280}
+              height={187}
+            />
+          </div>
+        </div>
+      </div>
+      {/* <button className='embla__prev' onClick={scrollPrev}>
+        Prev
+      </button>
+      <button className='embla__next' onClick={scrollNext}>
+        Next
+      </button> */}
+    </div>
   );
 };
-//  <Splide
-//    options={{
-//      perPage: 3,
-//      rewind: true,
-//      rewindSpeed: 200,
-//      width: '280px',
-//      height: '100%',
-//      direction: 'ttb',
-//      paginationDirection: 'ttb',
-//      focus: 'center',
-//      gap: '30px',
-//    }}
-//  >
-//    <SplideSlide>
-//      <Image src={mountain} alt='Mountain' width={280} height={187} />
-//    </SplideSlide>
-//    <SplideSlide>
-//      <Image src={lake} alt='Lake' width={280} height={187} />
-//    </SplideSlide>
-//    <SplideSlide>
-//      <Image src={forest} alt='Forest' width={280} height={187} />
-//    </SplideSlide>
-//  </Splide>;
+
 export default MobileGallery;
